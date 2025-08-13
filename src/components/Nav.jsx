91d0b426cfb9cb23f78b19e2803706e1100b7d0d
Nimra Khan <nk5476656@gmail.com>
2025-08-13 12:48:36 +0500

    Categories Section Completed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,7 +17,8 @@ const Nav = () => {
         text-[20px]'/>
       </form>
       <div className='w-[60px] h-[60px] bg-white flex justify-center items-center 
-      rounded-md shadow-xl'>
+      rounded-md shadow-xl relative'>
+        <span className='absolute top-0 right-2 text-green-500 font-bold text-[18px]'>0</span>
         <LuShoppingBag className='w-[30px] h-[30px] text-green-500'/>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import Categories from '../Category'
 
 const Home = () => {
   return (
     <div className='bg-slate-200 w-full min-h-screen'>
       <Nav />
+      <div className='flex flex-wrap justify-center items-center gap-5 w-full'>
+        {Categories.map((item) => {
+          return (
+            <div 
+              key={item.name}
+              className='w-[140px] h-[150px] bg-white flex flex-col 
+              items-center justify-center gap-3 text-[20px] font-semibold 
+              text-gray-600 rounded-lg shadow-xl hover:bg-green-200 
+              cursor-pointer transition-all duration-200'
+            >
+              {item.icon}
+              {item.name}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
